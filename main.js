8d54d5098a8d5796f43de74e24d8f1db5f7905f6
EgorYakovenko <Egor.Yakovenko@ukr.net>
2024-08-17 17:32:10 +0200

    practice

--- main.js
+++ main.js
@@ -47,33 +47,36 @@
 
 // ====================== O(n3) ======================
 
-// function multiplyMatrices(A, B) {
-//   const n = A.length;
-//   const C = new Array(n).fill(0).map(() => new Array(n).fill(0));
+function multiplyMatrices(A, B) {
+  const n = A.length;
+  const C = new Array(n).fill(0).map(() => new Array(n).fill(0));
 
-//   for (let i = 0; i < n; i++) {
-//     for (let j = 0; j < n; j++) {
-//       for (let k = 0; k < n; k++) {
-//         C[i][j] += A[i][k] * B[k][j];
-//       }
-//     }
-//   }
+  for (let i = 0; i < n; i++) {
+    console.log("i", i);
+    for (let j = 0; j < n; j++) {
+      console.log("j", j);
+      for (let k = 0; k < n; k++) {
+        console.log("k", k);
+        C[i][j] += A[i][k] * B[k][j];
+      }
+    }
+  }
 
-//   return C;
-// }
+  return C;
+}
 
-// const A = [
-//   [1, 2, 3],
-//   [4, 5, 6],
-//   [7, 8, 9],
-// ];
-// const B = [
-//   [10, 11, 12],
-//   [13, 14, 15],
-//   [16, 17, 18],
-// ];
+const A = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+const B = [
+  [10, 11, 12],
+  [13, 14, 15],
+  [16, 17, 18],
+];
 
-// console.log(multiplyMatrices(A, B));
+console.log(multiplyMatrices(A, B));
 
 //=====================================================
 // Пространственная сложность алгоритма O(1)
@@ -156,4 +159,4 @@ function peek(stack) {
   return stack[stack.length - 1];
 }
 const stack = ["a", "b", "c", "d"];
-console.log(peek(stack)); // Вивід: 'b'
+// console.log(peek(stack)); // Вивід: 'b'
